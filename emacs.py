07d609035e29047ebc95c285a814773a5bfc0dda
brian nie <niezp55@163.com>
2014-07-11 06:25:02 -0400

    add a python program to test the github sync

---
+++ emacs.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+data1 = np.random.randn(2,2)
+data3 = np.random.randn(2,2)
+print data1
+print data3
+
+def print_data(x):
+    print x
+
+def main():
+    print_data(data1)
+
+if __name__ =="__main__":
+    main()
+    
